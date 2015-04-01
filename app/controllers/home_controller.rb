26553be0266a0f7e671a0d615546c3489fca1a4b
esteban <efornal@gmail.com>
2015-04-01 12:19:17 -0300

    adedd employee search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,4 +4,11 @@ class HomeController < ApplicationController
     @offices = Office.all
   end
 
+
+  def search
+    text = params["text_search"]
+    @employees = Employee.where("name ilike ? or surname ilike ?", "%#{text}%", "%#{text}%")
+  end
+
+
 end
