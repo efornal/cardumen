0355ce306a743e6b5047c2cf43faa584e97d811b
esteban <efornal@gmail.com>
2015-04-09 09:34:29 -0300

    sorted results

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,13 +1,14 @@
 class HomeController < ApplicationController
 
   def index
-    @offices = Office.all
+    @offices = Office.all.order("name ASC")
   end
 
 
   def search
     text = params["text_search"]
     @employees = Employee.where("name ilike ? or surname ilike ?", "%#{text}%", "%#{text}%")
+                 .order("surname ASC")
   end
 
 
