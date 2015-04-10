dfe0f3155b04262b35241fe1d8d27bfb956a149f
esteban <efornal@gmail.com>
2015-04-10 12:54:46 -0300

    basic office search

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,9 +7,15 @@ class HomeController < ApplicationController
 
   def search
     @offices = Office.all.order("name ASC")
-    text = params["text_search"]
-    @employees = Employee.where("name ilike ? or surname ilike ?", "%#{text}%", "%#{text}%")
-                 .order("surname ASC")
+
+    if params['id']
+      @employees = Employee.where("office_id = ?", params['id']).order("surname ASC")
+    else
+      text = params["text_search"]
+      @employees = Employee.where("name ilike ? or surname ilike ?", "%#{text}%", "%#{text}%")
+                   .order("surname ASC")
+    end
+
   end
 
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'home/index'
   get 'home/search'
+  get 'home/search/:id' => 'home#search'
   devise_for :users
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   # The priority is based upon order of creation: first created -> highest priority.
